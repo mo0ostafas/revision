d5128db1c63ae85f476fbf322cf81e2f7c88b196
unknown <mustafayasser031212@gmail.com>
2025-02-19 20:04:03 +0200

    Fetch a JSON

--- jsmastery/003-json/app.js
+++ jsmastery/003-json/app.js
@@ -5,17 +5,18 @@ const quoteAuthor = document.querySelector('.quote-author');
 const quoteText = document.querySelector('.quote-text');
 quoteText.innerText = 'Click to generate a quote';
 
-// quotes fetching
-const options = {
-    method: 'GET',
-    headers: { 'X-Api-Key': 'NAwXDhtIa442zVFWLxiiq9n7nYzkm4zSROmbIH66' }
-}
-
-
+// app core feature
 const $ = async() => {
+    // emptying card content
     quoteCat.innerText = '';
     quoteAuthor.innerText = '';
     quoteText.innerText = 'loading...';
+    
+    // quotes fetching
+    const options = {
+        method: 'GET',
+        headers: { 'X-Api-Key': 'NAwXDhtIa442zVFWLxiiq9n7nYzkm4zSROmbIH66' }
+    }
     fetch('https://api.api-ninjas.com/v1/quotes', options)
         .then(res => res.json()) // to parse JSON into JS Object
         .then(data => {
